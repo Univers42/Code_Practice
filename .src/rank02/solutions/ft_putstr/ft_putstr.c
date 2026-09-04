2480a395bda57cce2c85e63017dd7e029ca7b05e
Casa <samuelperleo@gmail.com>
2026-09-04 17:57:46 +0200

    Soy un inutil. Ni un putstr bien hecho...

--- .src/rank02/solutions/ft_putstr/ft_putstr.c
+++ .src/rank02/solutions/ft_putstr/ft_putstr.c
@@ -1,8 +1,10 @@
 #include <unistd.h>
 
-void ft_putstr(char *str);
+void	ft_putstr(char *str)
 {
-    int i;
-    while(str[i])
-        write(1, &str[i++]);
+	int	i;
+
+	i = 0;
+	while (str[i])
+		write(1, &str[i++], 1);
 }
